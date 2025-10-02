01cad025997c697f095591e8a51cbdb514af6463
lukadscode <120412741+lukadscode@users.noreply.github.com>
2025-10-02 21:45:39 +0200

    Updated ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "octobre-rose-2025",
-      cwd: "/var/www/octobre-rose",
+      cwd: "/var/www/octobrerose",
       script: "server.js",
       exec_mode: "fork",
       instances: 1,
@@ -10,8 +10,8 @@ module.exports = {
         NODE_ENV: "production",
         PORT: 3003,
       },
-      error_file: "/var/www/octobre-rose/logs/pm2-error.log",
-      out_file: "/var/www/octobre-rose/logs/pm2-out.log",
+      error_file: "/var/www/octobrerose/logs/pm2-error.log",
+      out_file: "/var/www/octobrerose/logs/pm2-out.log",
       log_date_format: "YYYY-MM-DD HH:mm:ss Z",
     },
   ],
